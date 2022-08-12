f0f6e2ccbf92caa04feedcc38da29bf4d7e89f3e
Dhruv Kothari <60209337+dhruvdk7@users.noreply.github.com>
2022-08-12 11:10:45 +0530

    Add files via upload

---
+++ base/urls.py
@@ -0,0 +1,24 @@
+from django.urls import URLPattern, path
+from .views import RegisterPage, TaskDetail, TaskList,TaskCreate,TaskUpdate,DeleteView,CustomLoginView
+from .models import Task
+from django.contrib.auth.views import LogoutView
+
+
+urlpatterns=[
+    path('login/',CustomLoginView.as_view(),name="login"),
+    path('logout/',LogoutView.as_view(next_page='login'),name="logout"),
+    path('register/',RegisterPage.as_view(),name="register"),
+
+    path('',TaskList.as_view(),name='tasks'),
+    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
+    path('task-create/',TaskCreate.as_view(),name='task-create'),
+    path('task-update/<int:pk>/',TaskUpdate.as_view(),name='task-update'),
+    path('task-delete/<int:pk>/',DeleteView.as_view(),name='task-delete'),
+
+
+
+
+
+
+
+]
