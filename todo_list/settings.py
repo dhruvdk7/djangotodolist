381ba2cd7051c5b2c012cec1b6c174cf5fe7ee54
Dhruv Kothari <60209337+dhruvdk7@users.noreply.github.com>
2022-08-12 12:29:35 +0530

    Update settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l-nqr7hc9$27g990$z=%m&@&vn+r=up$n*lej$02gtw+stt%_f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://dktodolist.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
